165faf061a25805618361ad80318c45adfd9315f
virtue1803 <nguyenductai505@gmail.com>
2023-11-22 23:38:10 +0700

    thêm phương thức cho OderDetail

--- src/main/java/dev/week7_nguyenductai_21024541/backend/models/OrderDetail.java
+++ src/main/java/dev/week7_nguyenductai_21024541/backend/models/OrderDetail.java
@@ -1,4 +1,78 @@
 package dev.week7_nguyenductai_21024541.backend.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_detail")
 public class OrderDetail {
+    @Column(name = "quantity", nullable = false)
+    private double quantity;
+    @Column(name = "price", nullable = false)
+    private double price;
+    @Column(name = "note", length = 255, nullable = true)
+    private String note;
+
+    @EmbeddedId
+    private OrderDetailId orderDetailId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", updatable = false, insertable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", updatable = false, insertable = false)
+    private Product product;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(double quantity, double price, String note, Order order, Product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.note = note;
+        orderDetailId = new OrderDetailId(order, product);
+    }
+
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public OrderDetailId getOrderDetailId() {
+        return orderDetailId;
+    }
+
+    public void setOrderDetailId(OrderDetailId orderDetailId) {
+        this.orderDetailId = orderDetailId;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", note='" + note + '\'' +
+                ", orderDetailId='" + orderDetailId +
+                '}';
+    }
 }
