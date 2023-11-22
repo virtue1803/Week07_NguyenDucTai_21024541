d88412c1564c0251647801f1df7612afbf073542
virtue1803 <nguyenductai505@gmail.com>
2023-11-22 23:59:15 +0700

    Thêm phương iml cho service

--- src/main/java/dev/week7_nguyenductai_21024541/backend/services/IServices.java
+++ src/main/java/dev/week7_nguyenductai_21024541/backend/services/IServices.java
@@ -1,3 +1,8 @@
+package dev.week7_nguyenductai_21024541.backend.services;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IServices<T, ID> {
     void save(T t);
     void delete(ID id);
