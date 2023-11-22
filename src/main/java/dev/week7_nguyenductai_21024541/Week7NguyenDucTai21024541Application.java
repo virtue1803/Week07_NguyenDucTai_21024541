fa931da9ebcf362d857b93c7899b3c3780a6d91f
virtue1803 <nguyenductai505@gmail.com>
2023-11-22 23:08:59 +0700

    tạo dự án lab week 07

---
+++ src/main/java/dev/week7_nguyenductai_21024541/Week7NguyenDucTai21024541Application.java
@@ -0,0 +1,13 @@
+package dev.week7_nguyenductai_21024541;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7NguyenDucTai21024541Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week7NguyenDucTai21024541Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/week7_nguyenductai_21024541/Week7NguyenDucTai21024541ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.week7_nguyenductai_21024541;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7NguyenDucTai21024541ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
