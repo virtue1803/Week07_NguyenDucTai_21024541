cb78ec4cbcd367ff04749e75b2e14b4b1b4db908
virtue1803 <nguyenductai505@gmail.com>
2023-11-23 00:29:39 +0700

    chỉnh sửa web lần thứ 1

---
+++ src/main/java/dev/week7_nguyenductai_21024541/frontend/controller/TrangChuController.java
@@ -0,0 +1,14 @@
+package dev.week7_nguyenductai_21024541.frontend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TrangChuController {
+    @GetMapping("/home")
+    public String directToHome() {
+        return "index";
+    }
+    @GetMapping("/admin")
+    public String directToAdminManagement() {
+        return "admin/admin-homepage";
+    }
+}
